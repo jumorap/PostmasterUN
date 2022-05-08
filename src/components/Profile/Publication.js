36e4c1e681c562e1703306ade506220b0bd5e799
Jahel_Leon <jleonro@unal.edu.co>
2022-05-08 10:39:28 -0500

    Added saved publications modal

--- src/components/Profile/Publication.js
+++ src/components/Profile/Publication.js
@@ -1,29 +1,31 @@
-import { Favorite, FavoriteBorder } from '@mui/icons-material'
-import { Checkbox, Paper, Stack, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React, { useState } from 'react'
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import { Checkbox, Paper, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 
-
-export default function Publication({id, titulo, descripcion, fecha}) {
-    const [elevation, setElevation] = useState(1)
+export default function Publication({ title, description, date, onClick }) {
+  const [elevation, setElevation] = useState(1);
 
   return (
-    <Paper elevation={elevation} sx = {{p:2, position: "relative"}} onMouseOver={()=>setElevation(4)} onMouseOut = {()=>setElevation(1)}>
-        <Stack direction={"row"} spacing = {2} justifyContent = "space-between">
-        <Box sx={{"cursor": "pointer"}}>
-            <Typography variant='h4'>
-                Nombre de la publicacion
-            </Typography>
-            <Typography variant='body1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Mauris eget ipsum eget nunc varius aliquam.
-                Nulla facilisi.
-            </Typography>
+    <Paper
+      elevation={elevation}
+      sx={{ p: 2, position: "relative" }}
+      onMouseOver={() => setElevation(4)}
+      onMouseOut={() => setElevation(1)}
+    >
+      <Stack direction={"row"} spacing={2} justifyContent="space-between">
+        <Box sx={{ cursor: "pointer" }} onClick={onClick}>
+          <Typography variant="h4">{title}</Typography>
+          <Typography variant="body2">{date}</Typography>
+          <Typography variant="body1">{description}</Typography>
         </Box>
-        <Box display={"flex"} alignItems = "center">
-            <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite color='primary.strongRed'/>} />
+        <Box display={"flex"} alignItems="center">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite color="primary.strongRed" />}
+          />
         </Box>
-        </Stack>
+      </Stack>
     </Paper>
-  )
+  );
 }
--- src/sections/Profile.js
+++ src/sections/Profile.js
@@ -1,17 +1,111 @@
-import { Box } from '@mui/material'
-import React, { useState } from 'react'
-import {  PublicationList } from '../components'
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { PublicationList } from "../components";
+
+const informationList = [
+  {
+    type: "Postmaster",
+    title: "Primer lugar a investigación con sello UNAL",
+    description:
+      "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
+    tags: [
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+    ],
+    images: ["https://picsum.photos/500/550"],
+    links: [
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+    ],
+    favorite: false,
+    date: "2020-01-01",
+  },
+  {
+    type: "Postmaster",
+    title: "Primer lugar a investigación con sello UNAL",
+    description:
+      "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
+    tags: [
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+    ],
+    images: ["https://picsum.photos/500/550"],
+    links: [
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+    ],
+    favorite: false,
+    date: "2020-01-01",
+  },
+  {
+    type: "Postmaster",
+    title: "Primer lugar a investigación con sello UNAL",
+    description:
+      "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
+    tags: [
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+    ],
+    images: ["https://picsum.photos/500/550"],
+    links: [
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+    ],
+    favorite: false,
+    date: "2020-01-01",
+  },
+  {
+    type: "Postmaster",
+    title: "Primer lugar a investigación con sello UNAL",
+    description:
+      "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
+    tags: [
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+    ],
+    images: ["https://picsum.photos/500/550"],
+    links: [
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+    ],
+    favorite: false,
+    date: "2020-01-01",
+  },
+  {
+    type: "Postmaster",
+    title: "Primer lugar a investigación con sello UNAL",
+    description:
+      "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
+    tags: [
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+      { name: "UNAL", favorite: false },
+    ],
+    images: ["https://picsum.photos/500/550"],
+    links: [
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+      { name: "Clickable Link", url: "#basic-chip" },
+    ],
+    favorite: false,
+    date: "2020-01-01",
+  },
+];
 
 export default function Profile() {
-
-    const [list, setList] = useState([]);
-
+  const [list, setList] = useState([]);
 
   return (
     <Box>
-        <PublicationList list={["1", "2", "3"]}/>
+      <PublicationList list={informationList} />
     </Box>
-  )
+  );
 }
-
-
