2bdf6c46a32da6ab6f041a769b55ee5acdf45cc7
Jahel_Leon <jleonro@unal.edu.co>
2022-05-26 21:01:02 -0500

    added responsivenes to nav bar

--- src/components/Navbar/SideNavBar.js
+++ src/components/Navbar/SideNavBar.js
@@ -1,5 +1,5 @@
 import { Paper, Typography } from "@mui/material";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
@@ -14,7 +14,6 @@ import FirestoreManager from "../../../firebase/FirestoreManager";
 
 import IconButton from "@mui/material/IconButton";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-
 
 const areasFull = [
   "Postmaster",
@@ -31,7 +30,7 @@ const areasFull = [
   "Grupos estudiantiles",
 ];
 
-const  areas = ["Cargando...", ""];
+const areas = ["Cargando...", ""];
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -46,24 +45,21 @@ export default function SideNavBar({ open, handleDrawerClose, drawerWidth }) {
   const router = useRouter();
   const { dependency } = router.query;
 
-
-  const [dependenciesData, setDependenciesData] = useState(areas)
-  const [loaded, setLoaded] = useState(false)
+  const [dependenciesData, setDependenciesData] = useState(areas);
+  const [loaded, setLoaded] = useState(false);
 
   /***
    * Function that fetches the data from the firestore database
    */
   useEffect(() => {
     if (!loaded) {
-      dataQueryArray(FirestoreManager.getDependenciesList()).then(
-          (data) => {
-            const dataArray = data.map((item) => item.name);
-            setDependenciesData(dataArray);
-            setLoaded(true);
-          }
-      )
+      dataQueryArray(FirestoreManager.getDependenciesList()).then((data) => {
+        const dataArray = data.map((item) => item.name);
+        setDependenciesData(dataArray);
+        setLoaded(true);
+      });
     }
-  }, [loaded])
+  }, [loaded]);
 
   return (
     <Drawer
@@ -75,32 +71,44 @@ export default function SideNavBar({ open, handleDrawerClose, drawerWidth }) {
           boxSizing: "border-box",
         },
         zIndex: "100",
+        height: "100vh",
       }}
       variant="persistent"
       anchor="left"
       open={open}
+      PaperProps={{
+        style: {
+          height: "100vh",
+        }
+      }}
     >
-      <DrawerHeader>
-        <Typography variant="h6" noWrap>
-          Dependencias
-        </Typography>
-        <IconButton onClick={handleDrawerClose}>
-          <ChevronLeftIcon />
-        </IconButton>
-      </DrawerHeader>
-      <Divider />
-      <List dense>
-        {dependenciesData.map((text, index) => (
-          <Link href={`/${text}`} key={index}>
-            <ListItem button key={text} sx = {{backgroundColor: text === dependency?  "rgba(0, 0, 0, 0.1);" : "transparent"}}>
-              <ListItemIcon>{/*añadir despues*/}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-      <Divider />
-      
+        <DrawerHeader>
+          <Typography variant="h6" noWrap>
+            Dependencias
+          </Typography>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List dense>
+          {dependenciesData.map((text, index) => (
+            <Link href={`/${text}`} key={index}>
+              <ListItem
+                button
+                key={text}
+                sx={{
+                  backgroundColor:
+                    text === dependency ? "rgba(0, 0, 0, 0.1);" : "transparent",
+                }}
+              >
+                <ListItemIcon>{/*añadir despues*/}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+        <Divider />
     </Drawer>
   );
 }
