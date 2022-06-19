c0aa6703c2ae4e2d1a6a9670f9d4465079959748
Jahel_Leon <jleonro@unal.edu.co>
2022-06-18 22:49:04 -0500

    added favorite tags to profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,119 +1,31 @@
-import { Box, Stack, Typography, Paper, Avatar, Divider} from "@mui/material";
-import React, { useState, useEffect } from "react";
-import  SavedTags  from "./SavedTags";
+import { Box, Stack, Typography, Paper, Avatar, Divider } from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import SavedTags from "./SavedTags";
 import { InformationCard } from "../InformationCard";
 import PublicationList from "./PublicationList";
+import FirestoreManager from "../../../firebase/FirestoreManager";
+import { firebaseAppAuth } from "../../../firebase/firebase.config";
+import { AuthContext } from "../../../firebase/AuthProvider.config";
 
-import { firebaseAppAuth } from "../../../firebase/firebase.config"
 
-
-const informationList = [
-  {
-    type: "Postmaster",
-    title: "Graduación se realizará en las canchas de fútbol",
-    description:
-        "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
-    tags: [
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-    ],
-    images: ["https://picsum.photos/500/550"],
-    links: [
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-    ],
-    favorite: false,
-    date: "2020-01-01",
-  },
-  {
-    type: "Postmaster",
-    title: "Primer lugar a investigación con sello UNAL",
-    description:
-        "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
-    tags: [
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-    ],
-    images: ["https://picsum.photos/500/550"],
-    links: [
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-    ],
-    favorite: false,
-    date: "2020-01-01",
-  },
-  {
-    type: "Postmaster",
-    title: "Primer lugar a investigación con sello UNAL",
-    description:
-        "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
-    tags: [
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-    ],
-    images: ["https://picsum.photos/500/550"],
-    links: [
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-    ],
-    favorite: false,
-    date: "2020-01-01",
-  },
-  {
-    type: "Postmaster",
-    title: "Primer lugar a investigación con sello UNAL",
-    description:
-        "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
-    tags: [
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-    ],
-    images: ["https://picsum.photos/500/550"],
-    links: [
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-    ],
-    favorite: false,
-    date: "2020-01-01",
-  },
-  {
-    type: "Postmaster",
-    title: "Primer lugar a investigación con sello UNAL",
-    description:
-        "Esta es una pieza perteneciente a la campaña Orgullo UNAL, es de fondo azul con textos en blanco, tiene una fotografía donde se puede apreciar un procedimiento quirúrgico de apendicitis llevado a cabo por un equipo de cirujanos.",
-    tags: [
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-      { name: "UNAL", favorite: false },
-    ],
-    images: ["https://picsum.photos/500/550"],
-    links: [
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-      { name: "Clickable Link", url: "#basic-chip" },
-    ],
-    favorite: false,
-    date: "2020-01-01",
-  },
-];
 
 export default function Profile() {
-
-
   const [tags, setTags] = useState([
     { name: "Alemania", dependency: "DRE", id: "1" },
     { name: "Argentina", dependency: "DRE", id: "2" },
     { name: "Brasil", dependency: "DRE", id: "3" },
     { name: "Chile", dependency: "DRE", id: "4" },
   ]);
+
+  const [savedPublications, setSavedPublications] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    FirestoreManager.getFavoritePosts(currentUser.uid).then((posts) => {
+      console.log(posts);
+      setSavedPublications(posts);
+    });
+  }, []);
 
   function handleTagDelete(tag) {
     const newTags = tags.filter((t) => t.id !== tag.id);
@@ -133,26 +45,27 @@ export default function Profile() {
   });
 
   const selectItem = (index) => {
-    setCurrPubication(informationList[index]);
+    setCurrPubication(savedPublications[index]);
   };
 
   /*Firebase methods */
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
 
-
   // Check if user is admin to show createNews component
   useEffect(() => {
     firebaseAppAuth.onAuthStateChanged((u) => {
-      setIsUserAuthenticated(!!u?.email.toString().split('@')[1].includes('unal.edu.co'));
-    })
-  }, [])
+      setIsUserAuthenticated(
+        !!u?.email.toString().split("@")[1].includes("unal.edu.co")
+      );
+    });
+  }, []);
 
   // Get User Information
   const [user, setUser] = useState({
     src: "",
     name: "",
     email: "",
-    favPost: 1
+    favPost: 1,
   });
 
   useEffect(() => {
@@ -163,14 +76,14 @@ export default function Profile() {
             src: user.photoURL,
             name: user.displayName,
             email: user.email,
-            favPost: 5
+            favPost: 5,
           });
         } else {
           setUser({
             src: "",
             name: "",
             email: "",
-            favPost: 0
+            favPost: 0,
           });
         }
       });
@@ -179,51 +92,53 @@ export default function Profile() {
 
   console.log(isUserAuthenticated);
 
-
-
   // const display flex y none
   // if is admin -> display flex, else none
 
   return (
-      <Box sx={{paddingLeft: 10, paddingRight: 10}}>
+    <Box sx={{ paddingLeft: 10, paddingRight: 10 }}>
+      <Typography style={{ fontWeight: 500 }} variant="h4" gutterBottom>
+        Información del Usuario
+      </Typography>
 
-        <Typography style={{fontWeight: 500}} variant="h4" gutterBottom >
-          Información del Usuario
-        </Typography>
+      <Divider />
 
-        <Divider />
+      <Stack marginBottom={5} marginTop={2}>
+        <Paper elevation={4} sx={{ px: 2, py: 2 }}>
+          <Stack direction={"row"} alignItems={"center"} spacing={5}>
+            {/*Foto de perfil*/}
+            <Avatar sx={{ width: 150, height: 150 }} src={user.src} />
 
-        <Stack marginBottom={5} marginTop={2}>
-          <Paper elevation={4} sx = {{px:2, py: 2}}>
-            <Stack direction={'row'} alignItems={'center'} spacing={5} >
-              {/*Foto de perfil*/}
-              <Avatar sx={{ width: 150, height: 150 }} src={user.src} />
-
-              <Stack direction={'column'}>
-                <Typography style={{fontWeight: 600}} variant="h6">{user.name}</Typography>
-                <Typography variant="body2" color='#E51F1F' gutterBottom>{user.email}</Typography>
-                <Typography variant="body2">Estudiante</Typography>
-                <Typography variant="body2">Se unió el 23 de abril del 2022</Typography>
-                <Typography variant="body2">Publicaciones guardadas: {user.favPost}</Typography>
-              </Stack>
-
+            <Stack direction={"column"}>
+              <Typography style={{ fontWeight: 600 }} variant="h6">
+                {user.name}
+              </Typography>
+              <Typography variant="body2" color="#E51F1F" gutterBottom>
+                {user.email}
+              </Typography>
+              <Typography variant="body2">Estudiante</Typography>
+              <Typography variant="body2">
+                Se unió el 23 de abril del 2022
+              </Typography>
+              <Typography variant="body2">
+                Publicaciones guardadas: {user.favPost}
+              </Typography>
             </Stack>
-          </Paper>
-        </Stack>
+          </Stack>
+        </Paper>
+      </Stack>
 
-        <Typography variant="h4" gutterBottom color='#FF2525'>
-          Mis favoritos
-        </Typography>
+      <Typography variant="h4" gutterBottom color="#FF2525">
+        Mis favoritos
+      </Typography>
 
-        <Divider  color='#FFC8C8'/>
+      <Divider color="#FFC8C8" />
 
-        <Stack spacing={4} direction = {"column"} sx = {{py: 2}}>
-
-          <PublicationList list={informationList} selectItem = {selectItem}>
-            <InformationCard {...currPubication} />
-          </PublicationList>
-        </Stack>
-
-      </Box>
+      <Stack spacing={4} direction={"column"} sx={{ py: 2 }}>
+        <PublicationList list={savedPublications} selectItem={selectItem}>
+          <InformationCard {...currPubication} />
+        </PublicationList>
+      </Stack>
+    </Box>
   );
 }
